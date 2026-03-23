01a6376c555a6a6151ba12fb92e8ac3750e3d415
GowharAhmadMir <ergowhar00@gmail.com>
2026-03-23 20:30:32 +0530

    Add delete button functionality
    Added feature to delete tasks from the Todo App.

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 function addTask() {
   let input = document.getElementById("taskInput");
+
+  if (input.value === "") return;
+
   let li = document.createElement("li");
   li.textContent = input.value;
 
+  let btn = document.createElement("button");
+  btn.textContent = "❌";
+  btn.onclick = function () {
+    li.remove();
+  };
+
+  li.appendChild(btn);
   document.getElementById("taskList").appendChild(li);
+
   input.value = "";
-} 
+}
